65183c3d77b7a865a3b4f53b783e3d79d4f82ed8
ashenkumar@gmail.com <Yoshithadilmith@1121>
2023-05-24 10:33:39 +0530

    initial commit

---
+++ src/main/java/com/sliit/mr_mechanic/MrMechanicApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.mr_mechanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrMechanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrMechanicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/mr_mechanic/MrMechanicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.mr_mechanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrMechanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
